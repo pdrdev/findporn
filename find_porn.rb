e52328cbd4dcb89bdae1ef02783101f21b2919bf
pdrdev <prdsoftware@gmail.com>
2013-02-11 00:50:23 -0600

    find_hrefs method

--- find_porn.rb
+++ find_porn.rb
@@ -1,6 +1,10 @@
+require 'rubygems'
 require 'net/http'
+require 'nokogiri'
+
 require 'settings'
 require 'cookie_manager'
+require 'href'
 
 class FindPorn
 
@@ -19,9 +23,13 @@ class FindPorn
   end
 
   def do_search
+  end
+
+  def find_hrefs(query)
     http = Net::HTTP.new(@search_host)
-    response = http.post(@search_path, "max=1&to=1&nm=search_string&start=50", {'Cookie' => @cookie_manager.get_cookies})
-    write_result(response)
+    response = http.post(@search_path, "max=1&to=1&nm=#{query}&start=50", {'Cookie' => @cookie_manager.get_cookies})
+    doc = Nokogiri::HTML(response.body)
+    doc.xpath("//a[@class='med tLink bold']").map{|s| Href.new(s)}
   end
 
   def write_result (result)
