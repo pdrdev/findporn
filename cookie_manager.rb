e1fe3df43c77a664ed9361029a5156393c47d0f7
pdrdev <prdsoftware@gmail.com>
2013-02-10 20:19:01 -0600

    cookie manager and cookie manager spec

--- cookie_manager.rb
+++ cookie_manager.rb
@@ -1,3 +1,19 @@
 class CookieManager
-  # To change this template use File | Settings | File Templates.
+  attr_accessor :packed_cookies
+
+  def initialize
+    @packed_cookies = ''
+  end
+
+  def pack_cookies(cookie_array)
+    cookie_hash = {}
+    cookie_array.each { | cookie |
+      trimmed = cookie.split('; ')[0]
+      split = trimmed.split('=')
+      cookie_hash[split[0]] = split[1]
+    }
+
+    #sort so order will be deterministic
+    @packed_cookies = cookie_hash.map { |k, v| "#{k}=#{v}"}.sort.join('; ')
+  end
 end
--- spec/cookie_manager_spec.rb
+++ spec/cookie_manager_spec.rb
@@ -1,10 +1,23 @@
 require "rspec"
+require "cookie_manager"
 
-describe Cookie Manager do
+describe CookieManager do
 
-  it "should " do
+  before :each do
+    @cookie_manager = CookieManager.new
+  end
 
-    #To change this template use File | Settings | File Templates.
-    true.should == false
+  it "packs cookies" do
+    @cookie_manager.pack_cookies(
+        [
+            'phpbb3_r3qux_u=1; expires=Tue, 11-Feb-2014 01:34:32 GMT; path=/; HttpOnly',
+            'phpbb3_r3qux_k=; expires=Tue, 11-Feb-2014 01:34:32 GMT; path=/; HttpOnly',
+            'phpbb3_r3qux_sid=a93d1d77fcbb0b775901d8780d2768f3; expires=Tue, 11-Feb-2014 01:34:32 GMT; path=/; HttpOnly',
+            'phpbb3_r3qux_u=2; expires=Tue, 11-Feb-2014 01:34:32 GMT; path=/; HttpOnly',
+            'phpbb3_r3qux_k=; expires=Tue, 11-Feb-2014 01:34:32 GMT; path=/; HttpOnly',
+            'phpbb3_r3qux_sid=cdd6ac5f35b45aa3b582d1320f13d680; expires=Tue, 11-Feb-2014 01:34:32 GMT; path=/; HttpOnly'
+        ]
+    )
+    @cookie_manager.packed_cookies.should == 'phpbb3_r3qux_k=; phpbb3_r3qux_sid=cdd6ac5f35b45aa3b582d1320f13d680; phpbb3_r3qux_u=2'
   end
 end
