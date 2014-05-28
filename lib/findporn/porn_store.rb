eb80574ffbab19e9a25d3cbfd253946f54693443
lacungus <pdrdev@gmail.com>
2014-05-28 09:50:36 -0700

    append section appends to queries

--- lib/findporn/porn_store.rb
+++ lib/findporn/porn_store.rb
@@ -17,7 +17,7 @@ class PornStore
         i += 1
         Util.log("Processing query: #{query.value} (#{i}/#{total_queries})", true)
 
-        hrefs = pornolab_client.find_hrefs query
+        hrefs = pornolab_client.find_hrefs query + ' ' + section.append
         query.hrefs = hrefs
       end
     end
